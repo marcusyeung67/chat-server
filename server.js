667841dd49388ae0cc4fb688d62e3320c08222fd
marcusyeung67 <marcusmyeung2014@gmail.com>
2026-01-01 14:45:33 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-// server.js
 const WebSocket = require('ws');
+
 const wss = new WebSocket.Server({ port: 8080 });
 
 wss.on('connection', ws => {
     console.log("StickC connected");
 
     ws.on('message', msg => {
-        console.log("Received from StickC:", msg);
+        console.log("Received from StickC:", msg.toString());
     });
 });
